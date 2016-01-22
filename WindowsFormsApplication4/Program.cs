a693474e65200aa7fc49a2ba6f746026ceee91e6
matt monk <mmonk185@gmail.com>
2016-01-21 19:02:02 -0500

    First commit here

---
+++ WindowsFormsApplication4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // If I start to make changes
+            Application.Run(new Form1());
+        }
+    }
+}
